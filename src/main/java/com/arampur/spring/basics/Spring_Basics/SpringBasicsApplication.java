29afab8d494e5f91993340e56ff92e6d312599f9
RAMPUR-MAC <arampur@csu.fullerton.edu>
2019-06-05 01:02:10 -0700

    SpringFiles_Added

---
+++ src/main/java/com/arampur/spring/basics/Spring_Basics/SpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.arampur.spring.basics.Spring_Basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsApplication.class, args);
+	}
+
+}
